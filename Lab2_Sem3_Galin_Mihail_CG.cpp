80995e04332d743bdc12960bed29a2685ea3b43d
Mikhail Galin <mishagalin85@gmail.com>
2021-11-29 01:16:52 +0500

    Перенес на SFML, теперь точки задаются нажатием.

--- Lab2_Sem3_Galin_Mihail_CG.cpp
+++ Lab2_Sem3_Galin_Mihail_CG.cpp
@@ -1,65 +1,82 @@
-﻿#include <iostream>
-#include <windows.h>
+﻿#include "SFML/Graphics.hpp"
+#include <iostream>
 #include <vector>
 
+using namespace sf;
 using namespace std;
 
-HWND console = GetConsoleWindow();
-HDC hdc = GetDC(console);
-
-COLORREF blue = RGB(0, 0, 255);
-COLORREF green = RGB(0, 255, 0);
-COLORREF red = RGB(255, 0, 0);
-COLORREF white = RGB(255, 255, 255);
-
-const int n = 50;
-
-double getPt(double n1, double n2, double perc) {
+double getPt(const double& n1, const double& n2, const double& perc) {
 	return n1 + ((n2 - n1) * perc);
 }
 
-class Point {
-public:
-	double x = 0, y = 0;
-
-	Point() {
-		x = 50 + rand() % 500;
-		y = 50 + rand() % 500;
-	}
+struct Curve {
+	vector<vector<Vertex>> vertices;
+	int NumberOfVertices = 0;
 };
 
 int main()
 {
-	srand(unsigned(time(0)));
-	vector<vector<Point>> points;
+	Vector2i mousePos;
+	vector<Curve> curves(1);
+	vector<Vertex> points2;
+	RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
+	Event event;
 
-	while (true) {
-		system("cls");
-		points.clear();
-		for (int i = n; i > 0; --i) {
-			vector <Point> temp;
-			for (int j = i; j > 0; --j) {
-				Point tempPoint;
-				temp.push_back(tempPoint);
-			}
-			points.push_back(temp);
-		}
+	while (window.isOpen())
+	{
+		window.clear();
 
-		for (auto& point : points[0]) {
-			SetPixel(hdc, point.x, point.y, white);
-		}
+		while (window.pollEvent(event)) {
+			mousePos = Mouse::getPosition(window);
 
-		for (double i = 0; i < 1; i += 0.00001)
-		{
-			for (int j = 0; j < points.size(); j++) {
-				for (int k = 0; k < points[j].size() - 1; k++) {
-					points[j + 1][k].x = getPt(points[j][k].x, points[j][k + 1].x, i);
-					points[j + 1][k].y = getPt(points[j][k].y, points[j][k + 1].y, i);
+			if (event.type == Event::Closed) window.close();
+
+			if (event.type == Event::MouseButtonReleased && event.key.code == Mouse::Left) {
+				curves[curves.size() - 1].NumberOfVertices++;
+				curves[curves.size() - 1].vertices.clear();
+
+				Vertex tempPoint;
+				tempPoint.position.x = mousePos.x;
+				tempPoint.position.y = mousePos.y;
+
+				points2.push_back(tempPoint);
+				curves[curves.size() - 1].vertices.push_back(points2);
+				for (int i = curves[curves.size() - 1].NumberOfVertices - 1; i > 0; --i) {
+					vector<Vertex> temp;
+					for (int j = i; j > 0; --j) {
+						Vertex emptyVertex;
+						temp.push_back(emptyVertex);
+					}
+					curves[curves.size() - 1].vertices.push_back(temp);
 				}
 			}
 
-			SetPixel(hdc, points[points.size() - 1][0].x, points[points.size() - 1][0].y, white);
+			if (event.type == Event::MouseButtonReleased && event.key.code == Mouse::Right) {
+				Curve newCurve;
+				points2.clear();
+				points2.push_back(curves[curves.size() - 1].vertices[0][curves[curves.size() - 1].NumberOfVertices - 1]);
+				newCurve.vertices.push_back(points2);
+				newCurve.NumberOfVertices++;
+				curves.push_back(newCurve);
+			}
 		}
+
+		for (auto& curve : curves) {
+			if (curve.NumberOfVertices > 0) {
+				for (double i = 0; i <= 1.; i += 0.0001) {
+					for (int j = 0; j < curve.vertices.size() - 1; ++j) {
+						for (int k = 0; k < curve.vertices[j].size() - 1; ++k) {
+							curve.vertices[j + 1][k].position.x = getPt(curve.vertices[j][k].position.x, curve.vertices[j][k + 1].position.x, i);
+							curve.vertices[j + 1][k].position.y = getPt(curve.vertices[j][k].position.y, curve.vertices[j][k + 1].position.y, i);
+						}
+					}
+					window.draw(&curve.vertices[curve.vertices.size() - 1][0], 1, Points);
+				}
+			}
+		}
+
+		window.display();
 	}
+
 	return 0;
 }
